221e794fd24e1979893488daada0a7faeaa20bdf
GustavoAzHenrique <92117675+GustavoAzHenrique@users.noreply.github.com>
2022-11-07 17:45:17 -0300

    feat: primeiro commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6205feda79b16c91"; }
+/******/ 		__webpack_require__.h = function() { return "ba0598a54dea621a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,61 @@
+import config from "../aluraTube-config.json";
+import styled from "styled-components";
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+  const estiloHomePage = { backgroundColor: "red" };
+  //   const mensagem = "Testando identação";
+  console.log(config.playLists);
+
+  return (
+    <div style={estiloHomePage}>
+      {/* {mensagem} */}
+      <span>
+        <Menu />
+        <Header />
+        <TimeLine playlists={config.playLists} />
+      </span>
+    </div>
+  );
+}
+
+export default HomePage;
+
+function Menu() {
+  return <div>Menu</div>;
+}
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
   }
-  
-  export default HomePage
+  .user-info {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px;
+    gap: 16px;
+  }
+`;
+function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner" /> */}
+
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+}
+
+function TimeLine(props) {
+  console.log("Dentro do componente", props);
+  return <div>TimeLine</div>;
+}
